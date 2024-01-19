dc7f5a7c66923e65b4a62c8a593a8de5eba84415
Vũ Trung Hiếu <hieuvt@btc-studio.io>
2024-01-19 09:40:39 +0700

    order admin

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -14,3 +14,45 @@ export const getOrderController = async (req, res) => {
     return res.json(order);
   } catch (error) {}
 };
+
+export const getAllOrderController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate({
+        path: "product.id",
+        model: "Products",
+        select: "name price priceSale images",
+      })
+      .populate("buyer")
+      .sort({ createdAt: "-1" });
+    return res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error WHile Geting Orders",
+      error,
+    });
+  }
+};
+
+export const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Updateing Order",
+      error,
+    });
+  }
+};
--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { requireSignIn } from "../middlewares/authMiddleware.js";
-import { getOrderController } from "../controllers/orderController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {
+  getOrderController,
+  getAllOrderController,
+  orderStatusController
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
 router.get("/get-orders", requireSignIn, getOrderController);
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrderController);
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 
 export default router;
